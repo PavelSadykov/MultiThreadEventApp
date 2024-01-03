61346bb6b2cb7a3e6e1ab69a5a0b77d61338f4d3
PavelSadykov <2439747@gmail.com>
2024-01-03 13:18:22 +0500

    Добавьте файлы проекта.

---
+++ MultiThreadEventApp/Form1.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace MultiThreadEventApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnStart = new System.Windows.Forms.Button();
+            this.txtNumbersResult = new System.Windows.Forms.TextBox();
+            this.txtSumResult = new System.Windows.Forms.TextBox();
+            this.txtProductResult = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnStart
+            // 
+            this.btnStart.Location = new System.Drawing.Point(101, 78);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 0;
+            this.btnStart.Text = "Start";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click_1);
+            // 
+            // txtNumbersResult
+            // 
+            this.txtNumbersResult.Location = new System.Drawing.Point(221, 80);
+            this.txtNumbersResult.Multiline = true;
+            this.txtNumbersResult.Name = "txtNumbersResult";
+            this.txtNumbersResult.Size = new System.Drawing.Size(100, 188);
+            this.txtNumbersResult.TabIndex = 1;
+            // 
+            // txtSumResult
+            // 
+            this.txtSumResult.Location = new System.Drawing.Point(360, 80);
+            this.txtSumResult.Multiline = true;
+            this.txtSumResult.Name = "txtSumResult";
+            this.txtSumResult.Size = new System.Drawing.Size(100, 188);
+            this.txtSumResult.TabIndex = 2;
+            // 
+            // txtProductResult
+            // 
+            this.txtProductResult.Location = new System.Drawing.Point(495, 78);
+            this.txtProductResult.Multiline = true;
+            this.txtProductResult.Name = "txtProductResult";
+            this.txtProductResult.Size = new System.Drawing.Size(100, 190);
+            this.txtProductResult.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(221, 48);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(93, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Генерация чисел";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(357, 48);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(73, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Сумма чисел";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(492, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(113, 13);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Произведение чисел";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtProductResult);
+            this.Controls.Add(this.txtSumResult);
+            this.Controls.Add(this.txtNumbersResult);
+            this.Controls.Add(this.btnStart);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.TextBox txtNumbersResult;
+        private System.Windows.Forms.TextBox txtSumResult;
+        private System.Windows.Forms.TextBox txtProductResult;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+    }
+}
+
---
+++ MultiThreadEventApp/Form1.cs
@@ -0,0 +1,134 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace MultiThreadEventApp
+{
+    public partial class Form1 : Form
+    {
+       
+        private readonly string numbersFilePath = "numbers.txt";
+        private readonly string sumFilePath = "sums.txt";
+        private readonly string productFilePath = "products.txt";
+
+        private TaskCompletionSource<bool> generationTaskCompletionSource;
+        private TaskCompletionSource<bool> sumTaskCompletionSource;
+        private TaskCompletionSource<bool> productTaskCompletionSource;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async Task GenerateNumbers()
+        {
+            
+                using (StreamWriter file = new StreamWriter(numbersFilePath))
+                {
+                    Random random = new Random();
+                    for (int i = 0; i < 10; i++) // Генерируем 10 пар чисел
+                    {
+                        int num1 = random.Next(1, 11);
+                        int num2 = random.Next(1, 11);
+                        await file.WriteLineAsync($"{num1} {num2}");
+                       
+                    }
+                }
+
+            generationTaskCompletionSource.TrySetResult(true); 
+           
+        }
+
+        private async Task CalculateSum()
+        {
+           await generationTaskCompletionSource.Task;
+            
+                using (StreamReader file = new StreamReader(numbersFilePath))
+                using (StreamWriter sumFile = new StreamWriter(sumFilePath))
+                {
+                    string line;
+                    while ((line = await file.ReadLineAsync()) != null)
+                    {
+                        string[] numbers = line.Split(' ');
+                        int num1 = int.Parse(numbers[0]);
+                        int num2 = int.Parse(numbers[1]);
+                        
+                       await sumFile.WriteLineAsync($"{num1+num2}");
+                      
+                    }
+                }
+
+            sumTaskCompletionSource.TrySetResult(true);
+
+        }
+
+        private async Task CalculateProduct()
+        {
+            await sumTaskCompletionSource.Task;
+                using (StreamReader file = new StreamReader(numbersFilePath))
+                using (StreamWriter productFile = new StreamWriter(productFilePath))
+                {
+                    string line;
+                    while ((line = await file.ReadLineAsync()) != null)
+                    {
+                        string[] numbers = line.Split(' ');
+                        int num1 = int.Parse(numbers[0]);
+                        int num2 = int.Parse(numbers[1]);
+                        await  productFile.WriteLineAsync($"{num1 * num2}");
+                       
+                    }
+                }
+
+            productTaskCompletionSource.TrySetResult(true);
+
+
+        }
+
+        private async void  btnStart_Click_1(object sender, EventArgs e)
+        {
+            generationTaskCompletionSource = new TaskCompletionSource<bool>();
+            sumTaskCompletionSource = new TaskCompletionSource<bool>();
+            productTaskCompletionSource = new TaskCompletionSource<bool>();
+
+            Task generateTask = GenerateNumbers();
+          
+            Task sumTask = CalculateSum();
+           
+            Task productTask = CalculateProduct();
+
+            await generateTask;
+            await sumTask;
+            await productTask;
+
+            UpdateUIAfterCompletion();
+        }
+       private void UpdateUIAfterCompletion()
+        {
+            if(InvokeRequired)
+            {
+                this.Invoke(new Action(UpdateUIAfterCompletion));
+                return;
+                   
+            }
+
+            txtNumbersResult.Text = File.ReadAllText(numbersFilePath);
+            
+            txtSumResult.Text = File.ReadAllText(sumFilePath);
+           
+            txtProductResult.Text = File.ReadAllText(productFilePath);
+            MessageBox.Show("Операции завершены");
+                         
+            
+        }
+          
+    }
+}
